6008156185bf281a0325db3e3a507b156940efd4
Daiki Tanaka <67009309+tanktabox@users.noreply.github.com>
2022-06-20 23:34:56 +0900

    TODOの雛形の作成 JSXにてHTMLの雛形を作成

--- src/App.jsx
+++ src/App.jsx
@@ -2,5 +2,36 @@ import React from "react";
 import "./styles.css";
 
 export const App = () => {
-  return <div></div>;
+  return (
+    <>
+      <div>
+        <input placeholder="TODOを入力" />
+        <button>追加</button>
+      </div>
+      <div>
+        <p>未完了のTODO</p>
+        <ul>
+          <li>
+            <p>ああああ</p>
+            <button>完了</button>
+            <button>削除</button>
+          </li>
+          <li>
+            <p>いいいい</p>
+            <button>完了</button>
+            <button>削除</button>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <p>完了のTODO</p>
+        <ul>
+          <li>
+            <p>うううう</p>
+            <button>戻す</button>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
 };
