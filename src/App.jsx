80aac92d78a08147abbba2692064b2f84f2aac49
Daiki Tanaka <67009309+tanktabox@users.noreply.github.com>
2022-06-21 23:27:26 +0900

    todoアプリの実装

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,106 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 
 export const App = () => {
+  // 入力した値
+  const [todoText, setTodoText] = useState("");
+
+  // 未完了のTODOを格納する配列
+  const [incompleteTodos, setIncompleteTodos] = useState([]);
+  // 完了のTODOを格納する配列を定義
+  const [completeTodos, setCompleteTodos] = useState([]);
+
+  // inputに変更が生じるたびに中のvalueをtodoTextとして　setする
+  const onChangeTodoText = (event) => setTodoText(event.target.value);
+
+  // 追加ボタンがクリックされた時の処理
+  const onClickAdd = () => {
+    // todoTextが空文字の場合は処理をリターン→空文字のTODOは作れない
+    if (todoText === "") return;
+    // 現在のincompleteTodos配列を展開して新しい配列を作る、最後に現在のtodoTextを追加する
+    const newTodos = [...incompleteTodos, todoText];
+    // incompleteTodosの配列を(newTodos)で更新する
+    setIncompleteTodos(newTodos);
+    // inputのtodoTextにから文字を設定する
+    setTodoText("");
+  };
+
+  //　削除ボタンがクリックされた時の処理, map関数からindexで配列の順番を受け取る
+  const onClickDelete = (index) => {
+    // 現在のincompleteTodosを配列展開してnewTodosを作成
+    const newTodos = [...incompleteTodos];
+    //newTodosの中からクリックされたindexの要素を1つ削除(splice)
+    newTodos.splice(index, 1);
+    // setIncompleteTodosにnewTodos(削除済みの)を渡してIncompleteTodosを更新する
+    setIncompleteTodos(newTodos);
+  };
+
+  //　完了ボタンがクリックされた時の処理
+  const onClickComplete = (index) => {
+    //　onClickDeleteと一緒
+    // 現在のincompleteTodosを配列展開してnewIncompleteTodosを作成
+    const newIncompleteTodos = [...incompleteTodos];
+    //newIncompleteTodosの中からクリックされたindexの要素を1つ削除(splice)
+    newIncompleteTodos.splice(index, 1);
+
+    // newCompleteTodosは現在のcompleteTodosを配列展開してものの最後にincompleteTodosのクリックされたindexの要素を入れる
+    const newCompleteTodos = [...completeTodos, incompleteTodos[index]];
+
+    // stateの更新, incompleteTodosとcompleteTodos
+    setIncompleteTodos(newIncompleteTodos);
+    setCompleteTodos(newCompleteTodos);
+  };
+
+  // 戻すボタンがクリックされた時の処理
+  const onClickBack = (index) => {
+    const newCompleteTodos = [...completeTodos];
+    newCompleteTodos.splice(index, 1);
+
+    const newIncompleteTodos = [...incompleteTodos, completeTodos[index]];
+
+    setCompleteTodos(newCompleteTodos);
+    setIncompleteTodos(newIncompleteTodos);
+  };
+
   return (
     <>
-      <div>
-        <input placeholder="TODOを入力" />
-        <button>追加</button>
+      <div className="input-area">
+        <input
+          placeholder="TODOを入力"
+          value={todoText}
+          onChange={onChangeTodoText}
+        />
+        <button onClick={onClickAdd}>追加</button>
       </div>
-      <div>
-        <p>未完了のTODO</p>
+      <div className="incomplete-area">
+        <p className="title">未完了のTODO</p>
         <ul>
-          <li>
-            <p>ああああ</p>
-            <button>完了</button>
-            <button>削除</button>
-          </li>
-          <li>
-            <p>いいいい</p>
-            <button>完了</button>
-            <button>削除</button>
-          </li>
+          {incompleteTodos.map((todo, index) => {
+            return (
+              <li key={todo}>
+                <div className="list-row">
+                  <p>{todo}</p>
+                  <button onClick={() => onClickComplete(index)}>完了</button>
+                  <button onClick={() => onClickDelete(index)}>削除</button>
+                </div>
+              </li>
+            );
+          })}
         </ul>
       </div>
-      <div>
-        <p>完了のTODO</p>
+      <div className="complete-area">
+        <p className="title">完了のTODO</p>
         <ul>
-          <li>
-            <p>うううう</p>
-            <button>戻す</button>
-          </li>
+          {completeTodos.map((todo, index) => {
+            return (
+              <li key="todo">
+                <div className="list-row">
+                  <p>{todo}</p>
+                  <button onClick={() => onClickBack(index)}>戻す</button>
+                </div>
+              </li>
+            );
+          })}
         </ul>
       </div>
     </>
